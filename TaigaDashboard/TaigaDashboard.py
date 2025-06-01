1a54b12d6ad9078158fee4dd302edf585afb5b18
Jonah Largen <jhlargen2@gmail.com>
2025-06-01 15:44:06 -0400

    Remove Json from page

--- TaigaDashboard/TaigaDashboard.py
+++ TaigaDashboard/TaigaDashboard.py
@@ -7,7 +7,7 @@ from app.taiga_plotly import (
     get_task_assignment_heatmap_html,
     get_tag_cloud_html,
     get_tag_bar_chart_html,
-    get_issue_type_severity_priority_donut_charts_html
+    get_issue_type_severity_priority_donut_charts_html,
 )
 from dotenv import load_dotenv
 import json
@@ -17,8 +17,9 @@ app = Flask(__name__)
 
 load_dotenv()  # loads .env file into environment variables
 
-cache = Cache(config={'CACHE_TYPE': 'simple'})
+cache = Cache(config={"CACHE_TYPE": "simple"})
 cache.init_app(app)
+
 
 @app.route("/")
 @cache.cached(timeout=900)  # 900 seconds = 15 minutes
@@ -35,24 +36,17 @@ def home():
     priorities = client.get_priorities()
     issue_types = client.get_issue_types()
 
-    epics_json = json.dumps(epics, indent=2)
-    userstories_json = json.dumps(userstories, indent=2)
-    tasks_json = json.dumps(tasks, indent=2)
-    issues_json = json.dumps(issues, indent=2)
-    sprints_json = json.dumps(sprints, indent=2)
-    project_json = json.dumps(project, indent=2)
-    users_json = json.dumps(users, indent=2)
-    severities_json = json.dumps(severities, indent=2)
-    priorities_json = json.dumps(priorities, indent=2)
-    issue_types_json = json.dumps(issue_types, indent=2)
-
     project_name = project["name"]
     project_id = project["id"]
     logo = project["logo_small_url"]
     dashboard_config_html = get_dashboard_config_html()
     epic_progress_bar_html = get_epic_progress_html(epics, userstories)
     user_story_status_breakdown_html = get_task_status_breakdown_html(
-        userstories, [], [], sprints, "User Story Status Breakdown by Sprint (Requirement Items)"
+        userstories,
+        [],
+        [],
+        sprints,
+        "User Story Status Breakdown by Sprint (Requirement Items)",
     )
     task_status_breakdown_html = get_task_status_breakdown_html(
         [], tasks, issues, sprints, "Task/Issue Status Breakdown by Sprint (Work Items)"
@@ -62,7 +56,11 @@ def home():
     )
     tag_cloud_html = get_tag_cloud_html(userstories, tasks, issues)
     tag_bar_chart_html = get_tag_bar_chart_html(userstories, tasks, issues)
-    issue_type_severity_priority_donut_charts_html = get_issue_type_severity_priority_donut_charts_html(issues, issue_types, severities, priorities)
+    issue_type_severity_priority_donut_charts_html = (
+        get_issue_type_severity_priority_donut_charts_html(
+            issues, issue_types, severities, priorities
+        )
+    )
 
     return render_template(
         "index.html",
@@ -76,16 +74,6 @@ def home():
         tag_cloud_html=tag_cloud_html,
         tag_bar_chart_html=tag_bar_chart_html,
         issue_type_severity_priority_donut_charts_html=issue_type_severity_priority_donut_charts_html,
-        epics_json=epics_json,
-        userstories_json=userstories_json,
-        tasks_json=tasks_json,
-        issues_json=issues_json,
-        sprints_json=sprints_json,
-        project_json=project_json,
-        users_json=users_json,
-        severities_json=severities_json,
-        priorities_json=priorities_json,
-        issue_types_json=issue_types_json
     )
 
 
